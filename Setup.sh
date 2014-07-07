6c37bb3c7aa086c5ee3dac1fe70689be686a15e6
Simon Tsaoussis <newtonlabs@bellsouth.net>
2014-07-07 19:12:47 -0400

    Wifi Connectivity

--- Setup.sh
+++ Setup.sh
@@ -8,5 +8,9 @@ chmod 755 /etc/Car/android-usb-audio.py
 chmod 755 /etc/Car/phone_docked.sh
 cp /etc/Car/dock.rules /etc/udev/rules.d 
 cp /etc/Car/pulseaudio2 /etc/init
+cp /etc/Car/wpa_supplicant.conf /etc
+rm -f /etc/network/interfaces
+cp /etc/Car/interfaces /etc/network
+
 
 
